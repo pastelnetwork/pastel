ce9aea15423ebce5a48f8f45c868758b9a5a26a8
Wunder9l <vvarty@mail.ru>
2018-03-04 14:09:06 +0300

    Add test directory to Common services

---
+++ src/services/common_test/scheduler/TestTaskScheduler.cpp
@@ -0,0 +1,12 @@
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(TestTaskScheduler)
+
+    BOOST_AUTO_TEST_CASE(first_test) {
+        int i = 1;
+        BOOST_TEST(i);
+        BOOST_TEST(i == 2);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
