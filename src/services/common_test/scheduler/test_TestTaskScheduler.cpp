3bedc0930e43570b1873fdd1dc760c59ca9a02ea
Wunder9l <vvarty@mail.ru>
2018-03-15 23:29:34 +0300

    Add TestInappropriativeTask

--- src/services/common_test/scheduler/test_TestTaskScheduler.cpp
+++ src/services/common_test/scheduler/test_TestTaskScheduler.cpp
@@ -9,10 +9,21 @@ BOOST_AUTO_TEST_SUITE(TestTaskScheduler)
     BOOST_AUTO_TEST_CASE(inappropriative_task) {
         auto publisher = std::make_unique<services::TestTaskPublisher>(std::make_unique<services::JSONProtocol>());
         services::TestTaskScheduler scheduler(std::move(publisher));
+        scheduler.Run();
+        BOOST_CHECK_EQUAL(scheduler.AddTask(std::make_shared<services::FinishTask>()),
+                          services::AddTaskResult::ATR_Success);
 
         int i = 1;
         BOOST_TEST(i);
 //        BOOST_TEST(i == 2);
+    }
+
+    BOOST_AUTO_TEST_CASE(no_callback_set) {
+        auto publisher = std::make_unique<services::TestTaskPublisher>(std::make_unique<services::JSONProtocol>());
+        services::TestTaskScheduler scheduler(std::move(publisher));
+        scheduler.Run();
+        BOOST_CHECK_EQUAL(scheduler.AddTask(std::make_shared<services::FinishTask>()),
+                          services::AddTaskResult::ATR_ResponseCallbackNotSet);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
