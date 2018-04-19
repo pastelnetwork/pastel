a2ac2dea85aa4e06c1768b7424ba3af2d460f441
Jeff Emanuel <35050222+Dicklesworthstone@users.noreply.github.com>
2018-04-19 07:09:43 -0400

    Update anime_network.py

--- src/storage_layer/anime_network.py
+++ src/storage_layer/anime_network.py
@@ -1,8 +1,7 @@
  
-import sys, time, os.path, io, glob, hashlib, imghdr, random, os, sqlite3, socket, warnings, base64, json, hmac, logging, asyncio
+import sys, time, os.path, io, glob, hashlib, imghdr, random, os, zlib, pickle, sqlite3, uuid, socket, warnings, base64, json, hmac, logging, asyncio
 #import bencode, crc32c, dht, krpc, tracker, utils #TinyBT
 from shutil import copyfile
-#from fs.memoryfs import MemoryFS
 from random import randint
 from math import ceil, log, floor, sqrt
 from collections import defaultdict
@@ -10,7 +9,9 @@ from zipfile import ZipFile
 from subprocess import check_output
 from datetime import datetime
 from kademlia.network import Server
-from uuid import getnode as get_mac
+#from pyp2p.net import *
+#from pyp2p.unl import UNL
+#from pyp2p.dht_msg import DHT
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore",category=DeprecationWarning)
     from fs.copy import copy_fs
@@ -18,14 +19,26 @@ with warnings.catch_warnings():
 try:
     from tqdm import tqdm
     import yenc
-    import zstandard as zstd
+    import pyxolotl
 except:
     pass
-    
+try:
+    from socket import socketpair
+except ImportError:
+    from asyncio.windows_utils import socketpair
+#Requirements:
+# pip install kademlia, pyxolotl, yenc, tqdm, fs
+
+#Parameters:
+use_start_new_network = 0
+use_connect_to_existing_network = 0
+use_register_local_blocks_on_dht = 1 
+use_demonstrate_asking_network_for_file_blocks = 0
+use_demonstrate_transferring_block_file_using_nat_upnp = 0
+use_integrity_scan_of_block_files_on_startup = 0
 folder_path_of_art_folders_to_encode = 'C:\\animecoin\\art_folders_to_encode\\' #Each subfolder contains the various art files pertaining to a given art asset.
 block_storage_folder_path = 'C:\\animecoin\\art_block_storage\\'
 chunk_db_file_path = 'C:\\animecoin\\anime_chunkdb.sqlite'
-use_integrity_scan_of_block_files_on_startup = 0
 
 if use_integrity_scan_of_block_files_on_startup:
     if not os.path.exists(chunk_db_file_path):
@@ -94,7 +107,7 @@ if use_integrity_scan_of_block_files_on_startup:
 
 
 def generate_node_mac_hash_id():
-    return hashlib.sha256(str(int(get_mac())).encode('utf-8')).hexdigest()
+    return hashlib.sha256(str(int(uuid.getnode())).encode('utf-8')).hexdigest()
 
 def get_local_matching_blocks_from_file_hash_func(block_storage_folder_path,sha256_hash_of_desired_file):
     list_of_block_file_paths = glob.glob(os.path.join(block_storage_folder_path,'*'+sha256_hash_of_desired_file+'*.block'))
@@ -159,7 +172,68 @@ list_of_local_potential_file_hashes_unique = c.execute('SELECT DISTINCT file_has
 list_of_local_potential_file_hashes_unique = [x[0] for x in list_of_local_potential_file_hashes_unique]
 list_of_local_potential_file_hashes_unique_json = json.dumps(list_of_local_potential_file_hashes_unique)
 
+def package_hash_table_entry_func(data_as_python_list):
+    global default_port
+    my_node_id = generate_node_mac_hash_id()
+    data_as_python_list.insert(0,my_node_id)
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        my_node_ip_address = s.getsockname()[0]
+        s.close()
+    except Exception as e:
+        print('Error: '+ str(e)) 
+    my_node_ip_and_port = my_node_ip_address+ ':' + str(default_port)
+    data_as_python_list.insert(0,my_node_ip_and_port)
+    data_as_json = json.dumps(data_as_python_list)
+    data_as_json_zipped = zlib.compress(data_as_json.encode('utf-8'))
+    _, _, data_as_json_zipped_yenc_encoded = yenc.encode(data_as_json_zipped)
+    #print(data_as_json_zipped_yenc_encoded.decode('unicode-escape'))
+    return data_as_json_zipped_yenc_encoded
 
+    use_demonstrate_size_of_encodings = 0
+    if use_demonstrate_size_of_encodings:
+        data_as_json_zipped_base64_encoded = base64.b64encode(data_as_json_zipped)
+        size_of_data_as_json = sys.getsizeof(data_as_json)
+        size_of_data_as_json_zipped = sys.getsizeof(data_as_json_zipped)
+        size_of_data_as_json_zipped_yenc_encoded = sys.getsizeof(data_as_json_zipped_yenc_encoded)
+        size_of_data_as_json_zipped_base64_encoded = sys.getsizeof(data_as_json_zipped_base64_encoded)
+        with open('data_as_json','wb') as f:
+            f.write(data_as_json.encode('utf-8'))
+        with open('data_as_json_zipped','wb') as f:
+            f.write(data_as_json_zipped)   
+        with open('data_as_json_zipped_yenc_encoded','wb') as f:
+            f.write(data_as_json_zipped_yenc_encoded)
+        with open('data_as_json_zipped_base64_encoded','wb') as f:
+            f.write(data_as_json_zipped_base64_encoded)    
+        print('Comparison of the size of different representations of the data:\n')
+        print('data_as_json: '+str(size_of_data_as_json))
+        print('data_as_json_zipped: '+str(size_of_data_as_json_zipped))
+        print('data_as_json_zipped_yenc_encoded: '+str(size_of_data_as_json_zipped_yenc_encoded))
+        print('data_as_json_zipped_base64_encoded: '+str(size_of_data_as_json_zipped_base64_encoded))
+        print('Now verifying that we can decode the Yenc text and then unzip the data to get the original json:')
+        _, _, yenc_decoded = yenc.decode(data_as_json_zipped_yenc_encoded)
+        data_as_json_unzipped = zlib.decompress(yenc_decoded)
+        if yenc_decoded == data_as_json_zipped:
+            print('Decoded the Yenc successfully!')
+        if data_as_json_unzipped == data_as_json.encode('utf-8'):
+            print('Unzipped the data successfully!')
+
+
+def unpackage_hash_table_entry_func(data_as_yenc_encoded_zipped_data):
+    _, _, yenc_decoded = yenc.decode(data_as_yenc_encoded_zipped_data)
+    data_as_json_unzipped = zlib.decompress(yenc_decoded)
+    data_as_list_with_headers = json.loads(data_as_json_unzipped)
+    node_ip_and_port = data_as_list_with_headers[0]
+    node_ip = node_ip_and_port.split(':')[0]
+    node_port = int(node_ip_and_port.split(':')[1])
+    node_id = data_as_list_with_headers[1]
+    data_as_list = data_as_list_with_headers[2:]
+    return data_as_list, node_ip, node_port, node_id
+
+if 0:#Example usage:
+    yencoded_zipped_json = package_hash_table_entry_func(block_hash_list)
+    data_as_list, node_ip, node_port, node_id = unpackage_hash_table_entry_func(yencoded_zipped_json)
 
 def get_blocks_for_given_file_hash_func(file_hash):
     global chunk_db_file_path
@@ -170,164 +244,102 @@ def get_blocks_for_given_file_hash_func(file_hash):
     for current_block in blocks_for_this_file_hash:
         list_of_block_hashes.append(current_block[0])
     list_of_block_hashes_json = json.dumps(list_of_block_hashes)
-    return list_of_block_hashes, list_of_block_hashes_json
+    return list_of_block_hashes
 
-def advertise_my_local_blocks_func(server,list_of_local_potential_block_hashes):
-    global chunk_db_file_path
+def advertise_local_blocks_to_network(server):
     global default_port
     global alternative_port
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        my_node_ip_address = s.getsockname()[0]
-        s.close()
-    except Exception as e:
-        print('Error: '+ str(e)) 
-    my_node_ip_and_port = my_node_ip_address+ ':' + str(default_port)
-    list_of_local_potential_block_hashes.insert(0,my_node_ip_and_port)
-    my_node_id = generate_node_mac_hash_id()
-    try:
-        print('\nAdvertising to the network the '+str(len(list_of_local_potential_block_hashes))+' blocks held by the local machine, along with the local IP:port\n')
-        asyncio.get_event_loop().run_until_complete(server.set(my_node_id,list_of_local_potential_block_hashes))
-        time.sleep(1)
-    except:
-        pass
-    loop.run_until_complete(server.set(my_node_id,my_node_ip_address+':'+str(default_port))) #Announce who you are
-
+    global list_of_local_potential_block_hashes
+    global list_of_local_potential_file_hashes
+    global list_of_local_potential_file_hashes_unique
     
-def update_blockhash_to_zipfilehash_table_func(server,list_of_local_potential_block_hashes,list_of_local_potential_file_hashes,list_of_local_potential_file_hashes_unique):
     for cnt, current_file_hash in enumerate(list_of_local_potential_file_hashes_unique):
-        list_of_block_hashes, list_of_block_hashes_json = get_blocks_for_given_file_hash_func(current_file_hash)
+        list_of_block_hashes = get_blocks_for_given_file_hash_func(current_file_hash)
+        yencoded_zipped_json = package_hash_table_entry_func(list_of_block_hashes)
         try:
             print('Broadcasting to the network the list of blocks for the zip file with hash: '+current_file_hash)
-            asyncio.get_event_loop().run_until_complete(server.set(current_file_hash,list_of_block_hashes_json))
+            asyncio.get_event_loop().run_until_complete(server.set(current_file_hash,yencoded_zipped_json))
             time.sleep(1)
         except:
             pass
         
 def ask_network_for_blocks_corresponding_to_hash_of_given_zipfile_func(server,sha256_hash_of_desired_zipfile):
-        try:
-            print('\nAsking network for any block hashes that correspond to the zip file with the SHA256 hash: '+sha256_hash_of_desired_zipfile+'\n')
-            block_list = asyncio.get_event_loop().run_until_complete(server.get(sha256_hash_of_desired_zipfile))
-            time.sleep(1)
-            block_list = json.loads(block_list)
-        except:
-            pass
-        return block_list
-    
-def ask_network_for_nodeids_corresponding_to_hash_of_block_hash_func(server,sha256_hash_of_desired_block):
-        try:
-            print('\nAsking network for any nodeIDs that correspond to the block file with the SHA256 hash: '+sha256_hash_of_desired_block+'\n')
-            node_list = asyncio.get_event_loop().run_until_complete(server.get(sha256_hash_of_desired_block))
-            node_list = json.loads(node_list)
-        except:
-            pass
-        return node_list
- 
-def ask_network_for_node_information_func(desired_nodeid):
     try:
-        print('\nAsking network for the ip/port and the block list of the node with node_id: '+desired_nodeid+'\n')
-        node_info = asyncio.get_event_loop().run_until_complete(server.get(desired_nodeid))
-        time.sleep(1)
-        node_info = json.loads(node_info)
+        print('\nAsking network for any block hashes that correspond to the zip file with the SHA256 hash: '+sha256_hash_of_desired_zipfile+'\n')
+        yencoded_zipped_json = asyncio.get_event_loop().run_until_complete(server.get(sha256_hash_of_desired_zipfile))
+        time.sleep(5)
+        data_as_list, node_ip, node_port, node_id = unpackage_hash_table_entry_func(yencoded_zipped_json)
+        return data_as_list, node_ip, node_port, node_id
     except:
         pass
-    return node_info
 
-use_start_new_network = 0
-use_connect_to_existing_network = 0
-use_register_local_blocks_on_dht = 1 
-use_demonstrate_asking_network_for_file_blocks = 1
+def start_new_network_func(port):
+    try:
+        server = Server()
+        server.listen(port)
+        loop = asyncio.get_event_loop()
+        loop.set_debug(True)
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            server.stop()
+            loop.close()
+    except Exception as e:
+        print('Error: '+ str(e))
+
+def connect_to_existing_network(port):
+    try:
+        loop = asyncio.get_event_loop()
+        loop.set_debug(True)
+        server = Server()
+        server.listen(port)
+        return server, loop
+    except Exception as e:
+        print('Error: '+ str(e))
 
 if use_start_new_network:
-    server = Server()
-    try:
-        server.listen(default_port)
-        loop = asyncio.get_event_loop()
-        loop.set_debug(True)
-        try:
-            loop.run_forever()
-        except KeyboardInterrupt:
-            pass
-        finally:
-            server.stop()
-            loop.close()
-    except:
-        server.listen(alternative_port)
-        loop = asyncio.get_event_loop()
-        loop.set_debug(True)
-        try:
-            loop.run_forever()
-        except KeyboardInterrupt:
-            pass
-        finally:
-            server.stop()
-            loop.close()
+    start_new_network_func(alternative_port)  
 
 if use_connect_to_existing_network:
-    loop = asyncio.get_event_loop()
-    loop.set_debug(True)
-    server = Server()
-    
-    try:
-        server.listen(default_port)
-    except:
-        server.listen(alternative_port)
-        print('Problem!')
-    
+    server, loop = connect_to_existing_network(default_port)
     print('Bootstrapping network now!\n')
     loop.run_until_complete(server.bootstrap(bootstrap_node_list_filtered))
     time.sleep(10)
     
     if use_register_local_blocks_on_dht:
-        update_blockhash_to_zipfilehash_table_func(server,list_of_local_potential_block_hashes,list_of_local_potential_file_hashes,list_of_local_potential_file_hashes_unique)
-        time.sleep(10)
-        advertise_my_local_blocks_func(server,list_of_local_potential_block_hashes)
-        time.sleep(10)
-
+        advertise_local_blocks_to_network(server)
         
     if use_demonstrate_asking_network_for_file_blocks:
-        test_file_hash = 'a75a1757d54ad0876f9b2cf9b64b1017b6293ceed61e80cd64aae5abfdc8514e' #Arturo_number_04
+        #test_file_hash = 'a75a1757d54ad0876f9b2cf9b64b1017b6293ceed61e80cd64aae5abfdc8514e' #Arturo_number_04
+        #test_file_hash = '734ad8214cb20deab6474f19397d012678de4acc61c4c24de35ce3b8cc466d5f' #Mark_Kong_number_02
+        test_file_hash = '105c4c9b9d79ed544c36df792f7bbbddb6700209f51e4b9e1073fa41653d2aa8' #Iris Madula Number 11
+        
         print('Now asking network for hashes of blocks that correspond to the zipfile with a SHA256 hash of: '+test_file_hash)
-        block_hash_list = ask_network_for_blocks_corresponding_to_hash_of_given_zipfile_func(server,test_file_hash)
-        print('Sleeping...\n')
-        time.sleep(10)
-        list_of_node_ids = []
-        list_of_node_ids_lengths = []
-        for current_block_hash in block_hash_list:
-            current_node_id = ask_network_for_nodeids_corresponding_to_hash_of_block_hash_func(server,current_block_hash)
-            print('Sleeping...\n')
-            time.sleep(10)
-            if type(current_node_id) == 'str':
-                try:
-                    print('Node_ID: '+current_node_id+' has block with hash: '+current_block_hash)
-                    if current_node_id not in list_of_node_ids:
-                        list_of_node_ids.append(current_node_id)
-                        list_of_node_ids_lengths.append(len(current_node_id))
-                        if len(list_of_node_ids_lengths) > 4:
-                            if list_of_node_ids_lengths[-1] == list_of_node_ids_lengths[-2]:
-                                break
-                except Exception as e:
-                    print('Error: '+ str(e)) 
-                
-        list_of_node_ids = list(set(list_of_node_ids))
+        list_of_block_hashes, remote_node_ip, remote_node_port, remote_node_id = ask_network_for_blocks_corresponding_to_hash_of_given_zipfile_func(server,test_file_hash)
+        print('\n\nDone! Got back '+str(len(list_of_block_hashes))+' Block Hashes:\n')
+        print('\nNode IP:port is '+remote_node_ip+':'+str(remote_node_port))
+        print('\nNode ID is '+remote_node_id+'\n\n')
+        for current_block_hash in list_of_block_hashes:
+            print(current_block_hash+', ',end='')
+
+    if use_demonstrate_transferring_block_file_using_nat_upnp:
+         _, my_node_ip, my_node_port, my_node_id = unpackage_hash_table_entry_func( package_hash_table_entry_func([]))
         
-        list_of_ip_port_strings = []
-        
-        for current_node_id in list_of_node_ids:
-            node_info = ask_network_for_node_information_func(current_node_id)
-            ip_port_string = node_info[0]
-            if ip_port_string not in list_of_ip_port_strings:
-                list_of_ip_port_strings.append(ip_port_string)
+    if 0: #Experiments with PyP2P:
+        my_dht = DHT()
+        my_node_direct = Net(passive_bind=my_node_ip, passive_port=my_node_port, net_type='p2p', dht_node=my_dht, debug=1)
+        my_node_direct.start()
+        remote_dht = DHT()
+        remote_node_direct = Net(passive_bind=remote_node_ip, passive_port=remote_node_port, net_type='p2p', dht_node=remote_dht, debug=1)
+        remote_node_direct.start()
+        my_node_direct.unl.connect(bob_direct.unl.construct(), events)
+
         
 
-    if 0:
-        test_block_hash = '2755c37e3a3c9808d027348f48f889df7a5bac61cd0add15fa2d0a92da1ffb29'
-        result = loop.run_until_complete(server.get(test_block_hash))
-        print(result)
-    
-    if 0:
-        loop.call_soon_threadsafe(loop.stop)
-        server.stop()
-        loop.close()
+if 0:
+    loop.call_soon_threadsafe(loop.stop)
+    server.stop()
+    loop.close()
 
