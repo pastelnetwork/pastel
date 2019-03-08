a8767d8e5de31d2d9ca5929b9ee4a0bbc58a998b
vozdp <safihayed@it-simple.net>
2019-03-07 22:59:25 -0500

    Dead code has been removed

--- src/nsfw/NSFWImageChecker.cpp
+++
@@ -1,9 +0,0 @@
-#include "NSFWImageChecker.h"
-
-bool nsfw::NSFWImageChecker::IsAppropriateTask(std::shared_ptr<nsfw::ITask> &task) {
-    return task.get()->GetType() == TaskType::CheckNSFW;
-}
-
-void nsfw::NSFWImageChecker::HandleTask(std::shared_ptr<nsfw::ITask> &task) {
-    publisher.Send(task);
-}
