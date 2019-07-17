5f8d5cd8cdd65d3fffb0ef27b5e5dc002d1194af
vozdp <safihayed@it-simple.net>
2019-07-16 23:13:11 -0400

    WIP1 - Set/get fee; Masternode messages; paAddress/pyPubKey/pyCfg renamed to ext...

---
+++ src/mnode-messageproc.cpp
@@ -0,0 +1,211 @@
+// Copyright (c) 2018 The PASTELCoin Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "main.h"
+#include "key_io.h"
+
+#include "mnode-controller.h"
+#include "mnode-msgsigner.h"
+#include "mnode-messageproc.h"
+
+CCriticalSection cs_mapSeenMessages;
+CCriticalSection cs_mapOurMessages;
+CCriticalSection cs_mapLatestSender;
+
+bool CMasternodeMessage::Sign()
+{
+    sigTime = GetAdjustedTime();
+
+    std::string strError;
+    std::string strMessage = vinMasternodeFrom.prevout.ToStringShort() +
+                             vinMasternodeTo.prevout.ToStringShort() +
+                             vinMasternodeTo.prevout.ToStringShort() +
+                             std::to_string(sigTime) +
+                             message;
+
+    LogPrintf("CMasternodeMessage::Sign -- Message to sign: %s (%s)\n", ToString(), strMessage);
+
+    if(!CMessageSigner::SignMessage(strMessage, vchSig, masterNodeCtrl.activeMasternode.keyMasternode)) {
+        LogPrintf("CMasternodeMessage::Sign -- SignMessage() failed\n");
+        return false;
+    }
+
+    if(!CMessageSigner::VerifyMessage(masterNodeCtrl.activeMasternode.pubKeyMasternode, vchSig, strMessage, strError)) {
+        LogPrintf("CMasternodeMessage::Sign -- VerifyMessage() failed, error: %s\n", strError);
+        return false;
+    }
+
+    return true;
+}
+
+bool CMasternodeMessage::CheckSignature(const CPubKey& pubKeyMasternode, int &nDos)
+{
+    // do not ban by default
+    nDos = 0;
+
+    std::string strError;
+    std::string strMessage = vinMasternodeFrom.prevout.ToStringShort() +
+                             vinMasternodeTo.prevout.ToStringShort() +
+                             std::to_string(sigTime) +
+                             message;
+
+    LogPrintf("CMasternodeMessage::CheckSignature -- Message to check: %s (%s)\n", ToString(), strMessage);
+
+    if (!CMessageSigner::VerifyMessage(pubKeyMasternode, vchSig, strMessage, strError)) {
+        // Only ban for future block vote when we are already synced.
+        // Otherwise it could be the case when MN which signed this vote is using another key now
+        // and we have no idea about the old one.
+        if(masterNodeCtrl.masternodeSync.IsMasternodeListSynced()) {
+            nDos = 20;
+        }
+        return error("CMasternodeMessage::CheckSignature -- Got bad Masternode message from masternode=%s, error: %s", vinMasternodeFrom.prevout.ToStringShort().c_str(), strError);
+    }
+
+    return true;
+}
+
+void CMasternodeMessage::Relay()
+{
+    // Do not relay until fully synced
+    if(!masterNodeCtrl.masternodeSync.IsSynced()) {
+        LogPrintf("CMasternodeMessage::Relay -- won't relay until fully synced\n");
+        return;
+    }
+
+    LogPrintf("CMasternodeMessage::Relay -- Relaying {item} %s\n", GetHash().ToString());
+
+    CInv inv(MSG_MASTERNODE_MESSAGE, GetHash());
+    CNodeHelper::RelayInv(inv);
+}
+
+std::string CMasternodeMessage::ToString() const
+{
+
+    std::ostringstream info;
+    info << vinMasternodeFrom.prevout.ToStringShort() <<
+         ", " << vinMasternodeTo.prevout.ToStringShort() <<
+         ", " << sigTime <<
+         ", " << message <<
+         ", " << (int)vchSig.size();
+    return info.str();
+}
+
+
+void CMasternodeMessageProcessor::ProcessMessage(CNode* pfrom, std::string& strCommand, CDataStream& vRecv)
+{
+    if (strCommand == NetMsgType::MASTERNODEMESSAGE) {
+
+        CMasternodeMessage message;
+        vRecv >> message;
+
+        LogPrintf("MASTERNODEMESSAGE -- Get message %s from %d\n", message.ToString(), pfrom->id);
+
+        uint256 messageId = message.GetHash();
+
+        pfrom->setAskFor.erase(messageId);
+
+        if(!masterNodeCtrl.masternodeSync.IsMasternodeListSynced()) return;
+
+//        check
+//        cs_mapOurMessages
+
+        {
+            LOCK(cs_mapSeenMessages);
+
+            auto vi = mapSeenMessages.find(messageId);
+            if (vi != mapSeenMessages.end()) {
+                LogPrintf("MASTERNODEMESSAGE -- hash=%s, from=%s seen\n", messageId.ToString(), message.vinMasternodeFrom.ToString());
+                return;
+            }
+
+            mapSeenMessages[messageId] = message;
+            mapSeenMessages[messageId].MarkAsNotVerified(); // this removes signature in the message inside map, so we can skip this message from new syncs and as "seen"
+                                                            // but if message is correct it will replace the one inside the map
+        }
+
+//        if no vinMasternodeFrom - we only accept messages encrypted with our public key!!!!
+
+        masternode_info_t mnInfo;
+        if(!masterNodeCtrl.masternodeManager.GetMasternodeInfo(message.vinMasternodeFrom.prevout, mnInfo)) {
+            // mn was not found, so we can't check message, some info is probably missing
+            LogPrintf("MASTERNODEMESSAGE -- masternode is missing %s\n", message.vinMasternodeFrom.prevout.ToStringShort());
+            masterNodeCtrl.masternodeManager.AskForMN(pfrom, message.vinMasternodeFrom.prevout);
+            return;
+        }
+
+        int nDos = 0;
+        if(!message.CheckSignature(mnInfo.pubKeyMasternode, nDos)) {
+            if(nDos) {
+                LogPrintf("MASTERNODEMESSAGE -- ERROR: invalid signature\n");
+                Misbehaving(pfrom->GetId(), nDos);
+            } else {
+                LogPrintf("MASTERNODEMESSAGE -- WARNING: invalid signature\n");
+            }
+            // Either our info or vote info could be outdated.
+            // In case our info is outdated, ask for an update,
+            masterNodeCtrl.masternodeManager.AskForMN(pfrom, message.vinMasternodeFrom.prevout);
+            // but there is nothing we can do if vote info itself is outdated
+            // (i.e. it was signed by a mn which changed its key),
+            // so just quit here.
+            return;
+        }
+
+        //signature verified - replace with message with signature
+        {
+            LOCK(cs_mapSeenMessages);
+            mapSeenMessages[messageId] = message;
+        }
+        //Is it message to us?
+        bool bOurMessage = false;
+        if(masterNodeCtrl.IsMasterNode() && message.vinMasternodeTo.prevout == masterNodeCtrl.activeMasternode.outpoint && mnInfo.pubKeyMasternode == masterNodeCtrl.activeMasternode.pubKeyMasternode) {
+            LOCK(cs_mapOurMessages);
+            mapOurMessages[messageId] = message;
+            bOurMessage = true;
+        }
+
+        message.Relay();
+
+        //this is only if syncronization of messages is needed
+        //masterNodeCtrl.masternodeSync.BumpAssetLastTime("MASTERNODEMESSAGE");
+
+        LogPrintf("MASTERNODEMESSAGE -- %s message %s from %d.\n", bOurMessage? "Got": "Relaid", message.ToString(), pfrom->id);
+    }}
+
+void CMasternodeMessageProcessor::CheckAndRemove()
+{
+    if(!masterNodeCtrl.masternodeSync.IsBlockchainSynced()) return;
+
+    LOCK(cs_mapSeenMessages);
+
+    auto it = mapSeenMessages.begin();
+    while(it != mapSeenMessages.end()) {
+        CMasternodeMessage& item = (*it).second;
+
+        //remove old (1 day old?) stuff from Seen map - mapSeenMessages
+
+        // if () {
+        //     //process...
+        //     ++it;
+        // } else if() {
+        //     //remove...
+        //     mapWorkersTriads.erase(it++);
+        // } else {
+        //     ++it;
+        // }
+    }
+    LogPrintf("CMasternodeMessageProcessor::CheckAndRemove -- %s\n", ToString());
+}
+
+void CMasternodeMessageProcessor::Clear()
+{
+    LOCK(cs_mapSeenMessages);
+    mapSeenMessages.clear();
+}
+
+std::string CMasternodeMessageProcessor::ToString() const
+{
+    std::ostringstream info;
+    info << "Messages: " << (int)mapSeenMessages.size();
+    return info.str();
+}
---
+++ src/mnode-messageproc.h
@@ -0,0 +1,94 @@
+// Copyright (c) 2019 The PASTEL-Coin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+#ifndef MASTERNODEMESSAGEPROCESSOR_H
+#define MASTERNODEMESSAGEPROCESSOR_H
+
+
+#include "main.h"
+#include <map>
+
+extern CCriticalSection cs_mapSeenMessages;
+extern CCriticalSection cs_mapOurMessages;
+extern CCriticalSection cs_mapLatestSender;
+
+class CMasternodeMessage
+{
+
+public:
+    CTxIn vinMasternodeFrom;
+    CTxIn vinMasternodeTo;
+    std::string message;
+    int64_t sigTime; //message times
+    std::vector<unsigned char> vchSig;
+
+    CMasternodeMessage() = default;
+
+    CMasternodeMessage(COutPoint outpointMasternodeFrom, COutPoint outpointMasternodeTo, std::string& msg) :
+        vinMasternodeFrom(outpointMasternodeFrom),
+        vinMasternodeTo(outpointMasternodeTo),
+        sigTime(0),
+        message(msg)
+    {}
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        READWRITE(vinMasternodeFrom);
+        READWRITE(vinMasternodeTo);
+        READWRITE(message);
+        READWRITE(sigTime);
+        READWRITE(vchSig);
+    }
+
+    uint256 GetHash() const
+    {
+        CHashWriter ss(SER_GETHASH, PROTOCOL_VERSION);
+        ss << vinMasternodeFrom.prevout;
+        ss << vinMasternodeTo.prevout;
+        ss << sigTime;
+        ss << message;
+        return ss.GetHash();
+    }
+    bool IsVerified() { return !vchSig.empty(); }
+    void MarkAsNotVerified() { vchSig.clear(); }
+
+    bool Sign();
+    bool CheckSignature(const CPubKey& pubKeyMasternode, int &nDos);
+    void Relay();
+    std::string ToString() const;
+};
+
+class CMasternodeMessageProcessor {
+public:
+    std::map<uint256, CMasternodeMessage> mapSeenMessages;
+    std::map<uint256, CMasternodeMessage> mapOurMessages;
+
+//    TODO - DDoS protection
+//    std::map<uint256, > mapLatestSenders;
+//    std::map<CNetAddr, int64_t> mapLatestSenders; how many time during last hour(?) or time ago
+
+    CMasternodeMessageProcessor() {}
+
+    ADD_SERIALIZE_METHODS;
+
+    template<typename Stream, typename Operation>
+    inline void SerializationOp(Stream &s, Operation ser_action, int nType, int nVersion) {
+        LOCK(cs_mapSeenMessages);
+        READWRITE(mapSeenMessages);
+        READWRITE(mapOurMessages);
+    }
+
+public:
+    void ProcessMessage(CNode *pfrom, std::string &strCommand, CDataStream &vRecv);
+    void CheckAndRemove();
+    void Clear();
+    int SizeAll() { return mapSeenMessages.size(); }
+    int SizeOur() { return mapOurMessages.size(); }
+    std::string ToString() const;
+};
+
+#endif //MASTERNODEMESSAGEPROCESSOR_H
