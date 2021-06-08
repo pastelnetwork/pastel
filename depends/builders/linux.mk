1758948353cc1e85920406a02945673cce484cd2
akobrin1 <andrey.kobrin@gmail.com>
2021-06-07 23:58:45 -0400

    linux build fixes
    -rpc-tests.sh:
       -added -h|--help options to show usage help
    -support for test script parameters, parameters supported in group
    mode as well
    -show depends build info (build tools, host tools, package tools and
    flags)

--- depends/builders/linux.mk
+++ depends/builders/linux.mk
@@ -1,2 +1,5 @@
 build_linux_SHA256SUM = sha256sum
 build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -o
+
+build_linux_path=$(dir $(which gcc))
+build_linux_toolchain_path=$(if $(linux_install_path), $(linux_install_path),/usr/bin/)
