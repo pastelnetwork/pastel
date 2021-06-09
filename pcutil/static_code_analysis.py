1d52c8a42d3e584baf1d00107a68dc60aea2273d
akobrin1 <andrey.kobrin@gmail.com>
2021-06-08 23:23:15 -0400

    40391998: PVS Studio Analyzer use from command line
    -changed /pcutil/static_code_analysis.py to run PVS Studio Analyzer from
    command-line
    -added new parameter -j to specify job count to run analyzer, by default
    it is assigned to number of processors
    To generate license file locally you have to run script with "-e" and
    "-n" parameters only once.
    License file ~/.config/PVS-Studio/PVS-Studio.lic will be generated and
    used for all further runs.
    
    "./pcutil/static_code_analysis.py -e email -n key"
    where
       email: e-mail of the PVS Studio License
       key - PVS-Studio license number
    strace_out file should be generated previously in the root Pastel folder
    by build.sh script.
    Script then uses this file to run code analysis and generate html report
    in "fullhtml" subfolder.
    -"make analyze" from Pastel root folder will execute
    ./pcutil/static_code_analysis.py without parameters.

--- pcutil/static_code_analysis.py
+++ pcutil/static_code_analysis.py
@@ -4,59 +4,92 @@ import subprocess
 import sys
 import time
 import smtplib
+import shutil
+import multiprocessing
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 import os
 import argparse
 
 
-def main(pwd, from_mail , to_mail, pvs_mail,pvs_lic_nr):
+DEFAULT_EMAIL="pasteltest51@gmail.com"
+DEFAULT_SMTP_SERVER="smtp.gmail.com"
+DEFAULT_SMTP_PORT=587
+
+def main(pwd, from_mail , to_mail, pvs_mail, pvs_lic_nr, nJobCount):
     timestr = time.strftime("%Y%m%d_%H%M%S")
-    dirname = "static_code_analysis_{}".format(timestr)
+    dirname = f"static_code_analysis_{timestr}"
     passed = False
 
+    print("Pastel Static Code Analyzer (PVS Studio)")
     try:
-        subprocess.run(["mkdir","-p","~/.config/PVS-Studio"])
-        # Obtain license - give it a try here
-        subprocess.run(["pvs-studio-analyzer","credentials", pvs_mail ,pvs_lic_nr])
-
+        # get pastel root directory
+        scriptPath = os.path.dirname(os.path.realpath(__file__))
+        rootPath = os.path.dirname(scriptPath)
+        pvsConfigDir=os.path.expanduser("~/.config/PVS-Studio")
+        pvsLicenseFile=os.path.join(pvsConfigDir, 'PVS-Studio.lic')
+        print('Checking PVS Studio credentials...')
+        if not os.path.isfile(pvsLicenseFile):
+            print(f"PVS License file [{pvsLicenseFile}] does not exist, trying to generate it using supplied command-line parameters")
+            if pvs_mail is None:
+                raise ValueError('E-mail of the PVS Studio license is not defined, Please use -e parameter')
+            if pvs_lic_nr is None:
+                raise ValueError('PVS Studio license number is not defined. Please use -n parameter')
+            subprocess.run(["mkdir", "-p", "~/.config/PVS-Studio"])
+            # Obtain2 license - give it a try here
+            subprocess.run(["pvs-studio-analyzer", "credentials", "-o", pvsLicenseFile, pvs_mail, pvs_lic_nr], chec=True)
+            print(f"...PVS License file [{pvsLicenseFile}] created")
+        else:
+            print(f"...PVS License file [{pvsLicenseFile}] exists")
         # Create directory for actual log
-        subprocess.run(["mkdir","-p", "/pastel/qa/static_qa_report/{}".format(dirname)])
+        outDir = os.path.join(rootPath, "qa", "static_qa_report", dirname)
+        print(f'Output directory: {outDir}')
+        subprocess.run(["mkdir", "-p", outDir], check=True)
         
         # Filename is the same as directory name
         filename = dirname + ".log"
+        sOutputLogFile = os.path.join(outDir, filename)
 
         passed = True
+        if nJobCount is None:
+            nJobCount = multiprocessing.cpu_count()
 
+        sTraceFile = os.path.join(rootPath, "strace_out")
         # Run pvs-studio analyzer
-        subprocess.run(["pvs-studio-analyzer","analyze", "-l" ,"~/.config/PVS-Studio/PVS-Studio.lic", "-o", filename, "-j2"])
+        print(f'Executing PVS Studio analyzer ({nJobCount} jobs)...')
+        subprocess.run(["pvs-studio-analyzer", "analyze", "-l" , pvsLicenseFile, "-f", sTraceFile, "-o", sOutputLogFile, f"-j{nJobCount}"], check=True)
 
         # Run html report, compress and copy to artifacts location
-        zipped_name = "{}.tar.gz".format(dirname) 
-        subprocess.run(["plog-converter","-a", "GA:1,2" ,"-t", "fullhtml", filename, "-o", "./"])
+        zipped_name = f"{dirname}.tar.gz" 
+        print('Executing PVS Studio log converter...')
+        outHtmlPath = os.path.join(os.getcwd(), "fullhtml")
+        if os.path.exists(outHtmlPath) and os.path.isdir(outHtmlPath):
+            shutil.rmtree(outHtmlPath)
+        subprocess.run(["plog-converter","-a", "GA:1,2" ,"-t", "fullhtml", "-o", "./", sOutputLogFile])
+        print(f'HTML report is generated in {os.path.join(os.getcwd(), "fullhtml")}')
 
+        print(f'Compressing output directory ./fullhtml to {zipped_name}')
         subprocess.run(["tar","-zcf", zipped_name, "fullhtml"])
 
         #Copy to artifacts folder
-        subprocess.run(["mkdir","-p", "/pastel/pcutil/artifacts"])
-        subprocess.run(["cp", zipped_name, "/pastel/pcutil/artifacts"])
+        artifactsDir = os.path.join(scriptPath, "artifacts") 
+        subprocess.run(["mkdir", "-p", artifactsDir])
+        subprocess.run(["cp", zipped_name, artifactsDir])
 
-        # Send lofgile (without full report) if password is given
+        # Send logfile (without full report) if password is given
         if pwd is not None:
-
             if from_mail is None:
-                    from_mail = "pasteltest51@gmail.com"
-                    
+                    from_mail = DEFAULT_EMAIL
             if to_mail is None:
-                    to_mail= "pasteltest51@gmail.com"
+                    to_mail = DEFAULT_EMAIL
 
             msg = MIMEMultipart()
 
-            message = "Static code analysis:{}".format(filename)
+            message = f"Static code analysis:{filename}"
 
             msg['From'] = from_mail
             msg['To'] = to_mail
-            msg['Subject'] = "Static code analysis: {}".format(filename)
+            msg['Subject'] = f"Static code analysis: {filename}"
             password = pwd # SHALL be an input parameter
 
             part = None
@@ -73,15 +106,10 @@ def main(pwd, from_mail , to_mail, pvs_mail,pvs_lic_nr):
                 part['Content-Disposition'] = 'attachment; filename="%s"' % os.path.basename(f)
                 msg.attach(part)
 
-
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-
+            server = smtplib.SMTP(DEFAULT_SMTP_SERVER, DEFAULT_SMTP_PORT)
             server.starttls()
-
             server.login(msg['From'], password)
-
             server.sendmail(msg['From'], msg['To'], msg.as_string())
-
             server.quit()
 
     except Exception as bs:
@@ -95,13 +123,15 @@ def main(pwd, from_mail , to_mail, pvs_mail,pvs_lic_nr):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+
     parser.add_argument('-p', metavar = 'p', help='Pwd for mail server')
     parser.add_argument('-r', metavar = 'r', help='Log receiver mail address')
     parser.add_argument('-s', metavar = 's', help='Log sender mail address (gmail only)')
+    parser.add_argument('-j', metavar = 'j', help='Job count')
 
     parser.add_argument('-e', metavar = 'e', help='Email of PVS-Studio license')
-    parser.add_argument('-n', metavar = 'n', help='PVS-Studio license nr.')
+    parser.add_argument('-n', metavar = 'n', help='PVS-Studio license number.')
 
     args = parser.parse_args()
 
-    main(args.p,args.s,args.r, args.e, args.n)
+    main(args.p, args.s, args.r, args.e, args.n, args.j)
