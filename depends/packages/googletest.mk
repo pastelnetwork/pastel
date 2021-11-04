aba68ba3674930546ead51325c66f5687e8a70f2
akobrin1 <andrey.kobrin@gmail.com>
2021-11-04 19:21:41 -0400

    [43158891] Update ZeroMQ library to v4.3.4
    - updated libzmq library from v4.3.1 (01/12/2019) to v4.3.4 (01/17/2021)
    - changed depends package zeromq.mk to support new version,
    - converted ZeroMQ build from autotools to cmake
    - fixed some warnings in src/zmq related to ZeroMQ usage
    - Visual Studio 2019 build:
       - added new libzmq.props with ZeroMQ lib configuration
       - added libzmq.props to pasteld.props, pastel_gtest.props

--- depends/packages/googletest.mk
+++ depends/packages/googletest.mk
@@ -6,9 +6,13 @@ $(package)_download_file=release-$($(package)_version).tar.gz
 $(package)_sha256_hash=b4870bf121ff7795ba20d20bcdd8627b8e088f2d1dab299a031c1034eddc93d5
 
 define $(package)_set_vars
-$(package)_cxxflags+=-std=c++17
-$(package)_cxxflags_linux=-fPIC
-$(package)_cmake_opts_darwin=-DCMAKE_POLICY_DEFAULT_CMP0025=NEW
+$(package)_cxxflags+= -std=c++17
+$(package)_cxxflags_linux= -fPIC
+$(package)_cmake_opts+= -DCMAKE_CXX_STANDARD=17
+$(package)_cmake_opts+= -DCMAKE_CXX_STANDARD_REQUIRED=ON
+$(package)_cmake_opts+= -DCMAKE_CXX_EXTENSIONS=OFF
+$(package)_cmake_opts+= -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+$(package)_cmake_opts_darwin= -DCMAKE_POLICY_DEFAULT_CMP0025=NEW
 endef
 
 define $(package)_preprocess_cmds
@@ -17,12 +21,12 @@ endef
 
 define $(package)_config_cmds
   cd build && \
-  $($(package)_cmake) -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_CXX_EXTENSIONS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
+  $($(package)_cmake) ..
 endef
 
 define $(package)_build_cmds
   cd build && \
-  $(MAKE) VERBOSE=1
+  $(MAKE) -j$(JOBCOUNT) VERBOSE=1
 endef
 
 define $(package)_stage_cmds
