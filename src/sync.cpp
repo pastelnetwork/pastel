b58dec93df3ee49fa645b4db341b9cf90415bd8e
akobrin1 <andrey.kobrin@gmail.com>
2022-09-05 10:16:17 -0400

    build & circle-ci changes:
    - removed 6 duplicate jobs
    - configured single job build_platform that will be used to build release/debug for all platforms
    - build_platform job supports the following parameters:
       - platform: linux, darwin or windows
       - debug_mode: true|false
      - static_code_analysis: true|false - whether to run PVS static code analyzer
      - run_tests: true|false - execute tests
      - deploy: true|false deploy release to GitHub & AWS

--- src/sync.cpp
+++ src/sync.cpp
@@ -110,7 +110,7 @@ static void potential_deadlock_detected(const pair<void*, void*>& mismatch, cons
                 onlyMaybeDeadlock = true;
             secondLocked = true;
         }
-        LogPrintf(" %s\n", i.second.ToString());
+        LogPrintf(" %s\n", lockLocation.ToString());
     }
     assert(onlyMaybeDeadlock);
 }
