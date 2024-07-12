59f618c831c2842e80b1ee633a554ec8589b0f59
akobrin1 <andrey.kobrin@gmail.com>
2024-07-11 23:36:27 -0400

    compilation fix

--- src/extlibs/utfcpp/core.h
+++ src/extlibs/utfcpp/core.h
@@ -25,7 +25,7 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-#include "detect_cpp_standard.h"
+#include <utils/detect_cpp_standard.h>
 #include <iterator>
 
 #ifdef _HAS_CPP11_FEATURES // C++ 11 or later
