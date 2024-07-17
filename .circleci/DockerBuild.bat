b81f754550c5ae4b6e9a15c432b14d6747489e1c
akobrin1 <andrey.kobrin@gmail.com>
2024-07-17 18:54:18 -0400

    corrected builder image v0.0.8

--- .circleci/DockerBuild.bat
+++ .circleci/DockerBuild.bat
@@ -1,4 +1,4 @@
-docker build -t akobrin/pastel:0.0.7 .
+docker build -t akobrin/pastel:0.0.8 .
 
 rem docker run -it akobrin/pastel:0.0.3 /bin/bash
 rem git clone https://github.com/pastelnetwork/pastel.git .
